67ab4674adf0b9a0999d344b6f7d1c6b07a569ae
yusancky <umbrellacky@qq.com>
2023-01-11 11:13:03 +0800

    Support to get ChromeDriver version from Chrome version

--- webdriver_manager/chrome.py
+++ webdriver_manager/chrome.py
@@ -1,4 +1,7 @@
 import os
+import requests
+
+from re import fullmatch
 from typing import Optional
 
 from webdriver_manager.core.download_manager import DownloadManager
@@ -27,13 +30,35 @@ class ChromeDriverManager(DriverManager):
 
         self.driver = ChromeDriver(
             name=name,
-            version=version,
+            version=get_chromedriver_version(version),
             os_type=os_type,
             url=url,
             latest_release_url=latest_release_url,
             chrome_type=chrome_type,
             http_client=self.http_client,
         )
+
+    def get_chromedriver_version(chrome_version) -> str:
+        if fullmatch("[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*", chrome_version):
+            return chrome_version
+        elif fullmatch("[0-9]*\.[0-9]*\.[0-9]*", chrome_version) or fullmatch(
+            "[0-9]*", chrome_version
+        ):
+            version_response = requests.get(
+                f"https://chromedriver.storage.googleapis.com/LATEST_RELEASE_{chrome_version}"
+            )
+            if version_response.status_code == 200:
+                return version_response.text
+            elif version_response.status_code == 404:
+                raise ValueError(
+                    f"There is no such driver version number by url {version_response.url}"
+                )
+            else:
+                raise ValueError(
+                    f"response body:\n{version_response.text}\n"
+                    f"request url:\n{version_response.request.url}\n"
+                    f"response headers:\n{dict(version_response.headers)}\n"
+                )
 
     def install(self) -> str:
         driver_path = self._get_driver_path(self.driver)
